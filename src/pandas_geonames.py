480e4c17857b2bb68061fdc38f433c9667982235
Jan Tschada <jts@esri-de.com>
2019-08-03 15:02:59 +0200

    #1 Added the low_memory option

--- src/pandas_geonames.py
+++ src/pandas_geonames.py
@@ -8,4 +8,4 @@ class Geonames:
         self._header = 0
 
     def read(self, file_path):
-        return pd.read_csv(file_path, delimiter=self._delimiter, header=self._header)
+        return pd.read_csv(file_path, delimiter=self._delimiter, header=self._header, low_memory=False)
